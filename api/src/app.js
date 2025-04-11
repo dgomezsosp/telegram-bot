7f1ac1d6661e052772e4f216ecea063378937b2f
davidgosp <d.gomezsos@hotmail.com>
2025-04-11 15:27:16 +0200

    Creación de carpetas de controladores, middleware y error handler

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const errorHandler = require('./middlewares/error-handler')
 const routes = require('./routes')
 
 app.use(express.json({ limit: '10mb', extended: true }))
+
 app.use('/api', routes)
+app.use(errorHandler)
 
 module.exports = app
--- api/src/routes/admin/faqs.js
+++ api/src/routes/admin/faqs.js
@@ -1,30 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../../controllers/admin/faqs-controller.js')
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST request to the homepage FAQS')
-})
-
-router.get('/', (req, res) => {
-  console.log(req.query)
-  res.send('GET request to the homepage FAQS')
-})
-
-router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send('GET request to the homepage FAQS')
-})
-
-router.put('/:id', (req, res) => {
-  console.log(req.params.id)
-  console.log(req.body)
-  res.send('PUT request to the homepage FAQS with ID')
-})
-
-router.delete('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send('DELETE request to the homepage FAQS with ID')
-})
+router.post('/', controller.create)
+router.get('/', controller.findAll)
+router.get('/:id', controller.findOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
 module.exports = router
